0474c580ab14ad7cf90c850a3a615504e559f4e4
Kashin Son <jiachen0203@gmail.com>
2017-03-13 23:54:40 -0400

    Adapted js random function to price button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(function () {
+
+    var placeCheap = ["绿屋", "日本超市", "Piada", "麦当劳", "KFC", "Oxley", "餐车", "Wendy's", "Bibimbop"];
+    var placeMedium = ["Moe's", "Hongkong House", "Buckeye Pho", "丘记"];
+    var places = placeCheap;
     $(".price-button").click(function() {
         $(this).toggleClass("selected");
+        if ($(".selected").length === 0) {
+            $(this).toggleClass("selected");
+            return;
+        }
+        places = [];
+        $(".selected").each(function() {
+            if (this.id === "price-cheap") {
+                places = places.concat(placeCheap);
+            } else {
+                places = places.concat(placeMedium);
+            }
+            console.log(this.id);
+        });
+        console.log(places);
     });
-
-
     var set = new Set();
-    var places = ["绿屋", "日本超市", "Piada", "麦当劳", "KFC", "Wendy's", "Moe's", "Oxley", "餐车", "Hongkong House", "Buckeye Pho",
-        "丘记", "Bibimbop"];
     function roulette() {
-        if (set.size === places.length) set.clear();
+        if (set.size >= places.length) set.clear();
         var choice = Math.floor((places.length) * Math.random());
         if (set.has(places[choice])) {
             roulette();
@@ -16,6 +30,7 @@ $(function () {
             $("#roulette").html("<h1 align='center'>" + places[choice] + "</h1>");
             set.add(places[choice]);
         }
+        console.log(set);
     }
     roulette();
     $("#change-button").click(function() {
